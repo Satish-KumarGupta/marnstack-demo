60f1a35ab9a11c9ec07b96436de325f0e805e882
satish122 <satishkumar998475@gmail.com>
2021-10-18 23:04:20 +0530

    updated on heroku

--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const BlogPost =require('../Models/blogPost')
 
-router.get('/',(req,res)=>{
-    BlogPost.find({ })
-    .then((data)=>{
-        console.log('Data:',data)
-        res.json(data);
-    })
-    .catch((error)=>{
-        console.log('Error',error)
-    });
-
+// Routes
+router.get('/', (req, res) => {
+    BlogPost.find({  })
+        .then((data) => {
+            console.log('Data: ', data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log('error: ', daerrorta);
+        });
 });
 
 router.post('/save', (req, res) => {
@@ -29,6 +29,14 @@ router.post('/save', (req, res) => {
             msg: 'Your data has been saved!!!!!!'
         });
     });
+});
+
+router.get('/name', (req, res) => {
+    const data =  {
+        username: 'peterson',
+        age: 5
+    };
+    res.json(data);
 });
 
 
